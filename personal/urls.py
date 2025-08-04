66e9407e3adfd071fc8dafd232aa2d56931d2aac
cmendoza <cmendoza@gmail.com>
2025-08-04 16:07:38 -0500

    avance 06

--- personal/urls.py
+++ personal/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import login_view, logout_view, inicio
+from .views import login_view, logout_view, inicio, registro_usuario
 
 urlpatterns = [
     path('', login_view, name='login'),      # Página de inicio
     path('login/', login_view, name='login'),
     path('inicio/', inicio, name='inicio'),
     path('logout/', logout_view, name='logout'),
+    path('accounts/login/', login_view, name='login_redirect'),  # Redirección para login
+    # Puedes agregar más rutas aquí según sea necesario
+    path('registro/', registro_usuario, name='registro'),
 ]
 
--- personal/views.py
+++ personal/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 
 def login_view(request):
     if request.method == 'POST':
@@ -22,7 +23,17 @@ def login_view(request):
 def inicio(request):
     return render(request, 'inicio.html')  # Crea este archivo HTML también
 
-
 def logout_view(request):
     logout(request)
     return redirect('login')
+
+def registro_usuario(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # o a otra página que desees
+    else:
+        form = UserCreationForm()
+    return render(request, 'registrousuario.html', {'form': form})
+
